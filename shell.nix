4862fd48af9e36ab55acd03b11fa974f0e4626be
SBJ <swissbankingjew@proton.me>
2026-04-30 13:35:29 +0100

    fixed formatting - added comment

--- shell.nix
+++ shell.nix
@@ -4,22 +4,27 @@
   pkgs.mkShell {
     nativeBuildInputs = with pkgs; [
       gcc
-      openssl.dev
-      leveldb.dev
-      leveldb
       gnumake
       pkg-config
     ];
 
+    buildInputs = with pkgs; [
+      openssl.dev
+      leveldb.dev
+      leveldb
+    ];
+
+      
+    # The Makefile uses -march=native for hardware crypto instructions.
+    # This is acceptable for a developer shell on supported CPUs, but is not
+    # intended to provide binary-reproducible builds.      
     NIX_ENFORCE_NO_NATIVE = "0";
 
     SSL_PREFIX = "${pkgs.openssl.dev}";
     LDB_PREFIX = "${pkgs.leveldb.dev}";
 
     CPATH = "${pkgs.openssl.dev}/include:${pkgs.leveldb.dev}/include";
-
     LIBRARY_PATH = "${pkgs.openssl.out}/lib:${pkgs.leveldb.out}/lib";
 
-    CXXFLAGS = "-maes";
-
+    OCTRA_SKIP_AUTOSETUP=1;
   }
